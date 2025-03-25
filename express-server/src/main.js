b5746beefbaeb7c99a4a5443b4cffe701e947a93
Vladyslav <flickervladislav@gmail.com>
2025-03-25 21:54:47 +0200

    feat(express-app): add broken horizontal scaling

--- express-server/src/main.js
+++ express-server/src/main.js
@@ -1,3 +1,6 @@
+import cluster from 'node:cluster';
+import os from 'node:os';
+
 import express from 'express';
 
 import { ExitCode, PORT } from './constants.js';
@@ -7,30 +10,41 @@ import { createEventRepository } from './event/createEventRepository.js';
 import { createEventRouter } from './event/index.js';
 import { corsMiddleware, errorMiddleware } from './middlewares/index.js';
 
-const app = express();
+if (cluster.isPrimary) {
+  const serverInstantsCount = os.cpus().length - 1;
+  for (let i = 0; i < serverInstantsCount; i++) {
+    cluster.fork({ WORKER_PORT: PORT + i });
+  }
+}
 
-app.use(express.json());
-app.use(corsMiddleware);
+if (!cluster.isPrimary) {
+  const WORKER_PORT = process.env.WORKER_PORT;
 
-const eventRepository = createEventRepository(database);
-const eventController = createEventController(eventRepository);
-const eventRouter = createEventRouter(eventController);
+  const app = express();
 
-app.use('/events', eventRouter);
-app.use(errorMiddleware);
+  app.use(express.json());
+  app.use(corsMiddleware);
 
-const onListeningHandler = () => {
-  const url = `http://localhost:${PORT}`;
-  const serverStatusMessage = `Server is running on ${url}`;
-  console.log(serverStatusMessage);
-};
+  const eventRepository = createEventRepository(database);
+  const eventController = createEventController(eventRepository);
+  const eventRouter = createEventRouter(eventController);
 
-const onErrorHandler = (err) => {
-  console.error(`An error occurred: ${err.message}`);
-  process.exit(ExitCode.ERROR);
-};
+  app.use('/events', eventRouter);
+  app.use(errorMiddleware);
 
-app
-  .listen(PORT)
-  .on('listening', onListeningHandler)
-  .on('error', onErrorHandler);
+  const onListeningHandler = () => {
+    const url = `http://localhost:${WORKER_PORT}`;
+    const serverStatusMessage = `Server is running on ${url}`;
+    console.log(serverStatusMessage);
+  };
+
+  const onErrorHandler = (err) => {
+    console.error(`An error occurred: ${err.message}`);
+    process.exit(ExitCode.ERROR);
+  };
+
+  app
+    .listen(WORKER_PORT)
+    .on('listening', onListeningHandler)
+    .on('error', onErrorHandler);
+}
